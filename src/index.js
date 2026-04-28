cbefc040ee51f997a1e0eec86b6403c589d38e65
htsteelp1 <htsteelp1@gmail.com>
2026-04-28 12:24:49 -0400

    added about test document

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import {linkAbout} from "./pages/about.js";
 
 let content = document.querySelector("#content");
 
@@ -6,3 +7,4 @@ let  header = document.createElement("h1");
 header.innerText = "Test";
 header.id = "mainH"
 content.appendChild(header);
+console.log(linkAbout);
---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+
+
+let content = document.querySelector("#content");
+
+let aboutMe = document.createElement("p");
+aboutMe.innerText = "about";
+content.appendChild(aboutMe);
+
+export let linkAbout = "linkAbout";
