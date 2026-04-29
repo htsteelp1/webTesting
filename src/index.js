46baa2f849031c7c81c2270afe16c4ad20fd5bfc
htsteelp1 <htsteelp1@gmail.com>
2026-04-29 07:49:32 -0400

    Added some placeholder content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import {linkAbout} from "./pages/about.js";
+import {loadAbout} from "./pages/about.js";
 
 let body = document.querySelector("body");
 
@@ -7,4 +7,5 @@ let  header = document.createElement("h1");
 header.innerText = "Test";
 header.id = "mainH"
 body.insertBefore(header, body.firstChild)
-console.log(linkAbout);
+
+loadAbout();
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,24 @@
-
+import selfie from "../images/placeholder.png"
 
 let content = document.querySelector("#content");
 
-let aboutMe = document.createElement("p");
-aboutMe.innerText = "about";
-content.appendChild(aboutMe);
+function loadAbout() {
 
-export let linkAbout = "linkAbout";
+    let header = document.createElement("h2");
+    header.id = "aboutHeader";
+    header.innerText = "About Me";
+    content.appendChild(header);
+
+    let photo = document.createElement("img");
+    photo.src = selfie;
+    photo.id = "selfie";
+    content.appendChild(photo)
+
+    let blurb = document.createElement("div");
+    blurb.id = "blurb";
+    blurb.innerText = "Henry Steel Place Holder";
+    content.appendChild(blurb)
+
+}
+
+export {loadAbout};
